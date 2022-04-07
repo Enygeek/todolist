5779782eefdc7e6fefb5de5a5d04e1598d8a8bd2
Enygeek <61242498+Enygeek@users.noreply.github.com>
2022-04-07 17:41:39 +0200

    Introduction au fragment
    Fragment React :+1:

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-const ToDoList = () =>
-    (
+const ToDoList = () => (
+    <React.Fragment>
+        <h1 className="m-3">Liste de tâches</h1>
         <ul className="list-group m-3">
             <li className="list-group-item d-flex align-tiems-center">
                 Ranger la vaisselle
@@ -20,6 +21,8 @@ const ToDoList = () =>
                 <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
             </li>
         </ul>
-    )
+    </React.Fragment>
+)
+
 
 export default ToDoList
